14c72dc76926ecc4a628bf73e60775693484a4f7
Caleb Spain <crspain@uh.edu>
2020-03-18 19:24:25 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Testing</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#077525',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 60,
+    textAlign: 'center',
+    color: '#fff'
+  }
+});
